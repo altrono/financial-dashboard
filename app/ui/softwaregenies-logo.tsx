8f5f93ad5526c11d08425c4bab82b6e1606eaa90
altrono <hbotema16@gmail.com>
2024-03-24 20:30:44 +0200

    Create dahsboard layout and pages

--- app/ui/softwaregenies-logo.tsx
+++ app/ui/softwaregenies-logo.tsx
@@ -7,7 +7,7 @@ export default function SoftwaregeniesLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Softwaregenies</p>
+      <p className="text-[16px]">Softwaregenies</p>
     </div>
   );
 }
