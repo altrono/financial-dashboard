250a8ce2269e78ab4e3aa8e3f6374c14a40a22de
altrono <hbotema16@gmail.com>
2024-03-31 16:44:46 +0200

    Validate and prepare the data with zod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,19 @@
 'use server';
+import {z} from 'zod';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string()
+    
+})
+
+const CreateInvoice = FormSchema.omit({
+    id: true,
+    date: true
+})
 
 export async function createInvoice(formData: FormData) {
     // const rawFormData = {
@@ -7,7 +22,17 @@ export async function createInvoice(formData: FormData) {
     //     status: formData.get('status'),
     //   };
 
-    const rawFormData = Object.fromEntries(formData.entries())
+    // const rawFormData = Object.fromEntries(formData.entries())
+    const { customerId, amount, status} = CreateInvoice.parse({
+            customerId: formData.get('customerId'),
+            amount: formData.get('amount'),
+            status: formData.get('status'),
+          });
 
-    console.log(rawFormData);
+          const amountIncent = amount * 100;
+          console.log(new Date().toISOString())
+          const data = new Date().toISOString().split("T")[0];
+
+
+    
 }
